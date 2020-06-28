c472eb17ade25f7c280c3e7ae1fb7c0846ab75fc
Jack Madden <jmadden@astro.cornell.edu>
2020-06-28 14:54:55 -0400

    Create header.js

---
+++ header.js
@@ -0,0 +1,34 @@
+					<h1 id="logo"><a href="index.html">Home</a></h1>
+					<nav id="nav">
+						<ul>
+							<!-- <li><a href="index.html">Home</a></li> -->
+							<!-- <li>
+								<a href="#">Layouts</a>
+								<ul>
+									<li><a href="left-sidebar.html">Left Sidebar</a></li>
+									<li><a href="right-sidebar.html">Right Sidebar</a></li>
+									<li><a href="no-sidebar.html">No Sidebar</a></li>
+									<li>
+										<a href="#">Submenu</a>
+										<ul>
+											<li><a href="#">Option 1</a></li>
+											<li><a href="#">Option 2</a></li>
+											<li><a href="#">Option 3</a></li>
+											<li><a href="#">Option 4</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li> -->
+							<li><a href="https://jmadden.artstation.com/">Art</a></li>
+							<li><a href="images/resume.pdf">CV</a></li>
+							<li><a href="publications.html">Publications</a></li>
+							<!-- <li><a href="#" class="button primary">Contact</a></li> -->
+
+							<li>
+								<a class="button primary">Contact</a>
+								<ul>
+									<li><a href="mailto:jmadden@astro.cornell.edu">jmadden@astro.cornell.edu</a></li>
+								</ul>
+							</li>
+						</ul>
+					</nav>
