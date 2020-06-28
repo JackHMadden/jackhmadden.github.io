fb45fd90e733fe1ed041a571fc7919aff3c35441
Jack Madden <jmadden@astro.cornell.edu>
2020-06-28 13:54:12 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<header id="header">
+					<h1 id="logo"><a href="index.html">Home</a></h1>
+					<nav id="nav">
+						<ul>
+            
+							<li><a href="https://jmadden.artstation.com/">Art</a></li>
+							<li><a href="images/resume.pdf">CV</a></li>
+							<li><a href="publications.html">Publications</a></li>
+							<li>
+								<a class="button primary">Contact</a>  
+								<ul>
+									<li><a href="mailto:jmadden@astro.cornell.edu">jmadden@astro.cornell.edu</a></li>
+								</ul>
+                
+							</li>
+						</ul>
+					</nav>
+				</header>
